d1320f275d12b6034d3517970529f08039bc59b3
Maryam <mariamly98@yahoo.com>
2021-03-29 18:27:46 +0300

    sponsers

--- src/components/Sponsers.jsx
+++ src/components/Sponsers.jsx
@@ -2,28 +2,55 @@ import React from "react";
 
 function Sponsers() {
   return (
-    <div className="about">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Sponsers</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        </div>
-      </div>
+    <div className="container">
+      <div className="brief-conatiner "></div>
+    <div className="sponser-head">
+    <h1><span className="dash-color"> - </span>Magnificent events deserve magnificent sponsors<span className="dash-color"> - </span></h1>
+    <p className="sponser-quote">We extend a warm thank you for our Sponsors that honoured us in our events</p>
+</div>
+<div className="sponser-grid">
+   
+    <div className="sponser-card">
+        <img src="rolex.jpg" alt="" />
+        <span>Rolex</span>
     </div>
+    
+    <div className="sponser-card">
+        <img src="sample.png" alt="" />
+        <span>Sponser Name</span>
+    </div>
+   
+    <div className="sponser-card">
+        <img src="sample.png" alt="" />
+        <span>Sponser Name</span>
+    </div>
+    
+    <div className="sponser-card">
+        <img src="sample.png" alt="" />
+        <span>Sponser Name</span>
+    </div>
+    
+    <div className="sponser-card">
+        <img src="sample.png" alt="" />
+        <span>Sponser Name</span>
+    </div>
+   
+    <div className="sponser-card">
+        <img src="sample.png" alt="" />
+        <span>Sponser Name</span>
+    </div>
+    
+    <div className="sponser-card">
+        <img src="sample.png" alt="" />
+        <span>Sponser Name</span>
+    </div>
+    
+    <div className="sponser-card">
+        <img src="sample.png" alt="" />
+        <span>Sponser Name</span>
+    </div>
+</div>
+</div>
   );
 }
 
