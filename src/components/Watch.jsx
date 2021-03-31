6e50953a4de8bea616a17fffb6a638d612e17349
ahmedibrahiim287 <49068254+ahmedibrahiim287@users.noreply.github.com>
2021-03-31 03:24:09 +0200

    watch

--- src/components/Watch.jsx
+++ src/components/Watch.jsx
@@ -5,7 +5,7 @@ function Watch() {
     <Fragment>
     
 
-    <section className="container boot-elemant-bg py-md-5 py-4" >
+    <section className="padd container boot-elemant-bg py-md-5 py-4 poss " >
     
         <div className="container position-relative py-md-5 py-0">
         <div className="brief-conatiner "></div>
@@ -24,6 +24,44 @@ function Watch() {
         <div className="elemant-bg-overlay black"></div>
     </section>
 
+
+    <section className="padd container boot-elemant-bg py-md-5 py-4 posss " >
+    
+        <div className="container position-relative py-md-5 py-0">
+        <div className="brief-conatiner "></div>
+        <div className="brief-conatiner "></div>
+    
+                    <div className="row">
+                <div className="col-lg-8">
+                    <span className="text-black text-uppercase  ">Tedx helwan </span>
+                    <h2 className="text-white display-3 font-weight-bold">Tangram event</h2>
+                    <p className="f-w-16 mb-4">10/4/2021 </p>
+                    <a href="./Sponsers" className="btn btnn  btn-outline-white btn-lg px-4"> Read more </a>
+                    <a href="./Sponsers" className="btn btn-outline-white btn-lg px-4"> <i className="fab fa-facebook-f"></i> </a>
+                </div>
+            </div>
+        </div>
+        <div className="elemant-bg-overlay black"></div>
+    </section>
+
+    <section className="padd container boot-elemant-bg py-md-5 py-4 pos poss " >
+    
+        <div className="container position-relative py-md-5 py-0">
+        <div className="brief-conatiner "></div>
+        <div className="brief-conatiner "></div>
+    
+                    <div className="row">
+                <div className="col-lg-8">
+                    <span className="text-black text-uppercase  ">Tedx helwan </span>
+                    <h2 className="text-white display-3 font-weight-bold">Tangram event</h2>
+                    <p className="f-w-16 mb-4">10/4/2021 </p>
+                    <a href="./Sponsers" className="btn btnn  btn-outline-white btn-lg px-4"> Read more </a>
+                    <a href="./Sponsers" className="btn btn-outline-white btn-lg px-4"> <i className="fab fa-facebook-f"></i> </a>
+                </div>
+            </div>
+        </div>
+        <div className="elemant-bg-overlay black"></div>
+    </section>
 
 
     </Fragment>
