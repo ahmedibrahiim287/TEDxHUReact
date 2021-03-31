6bd40a2825154ae1e0c71e28105e7f0002bf9e42
ahmedibrahiim287 <49068254+ahmedibrahiim287@users.noreply.github.com>
2021-03-31 19:22:54 +0200

    about edit

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import axios from 'axios';
+import { Fragment } from "react";
 
 export class Contactus extends Component {
 
@@ -47,61 +48,12 @@ export class Contactus extends Component {
 
   render() {
   return (
-    <div class="container">
-       <br/>
-       <br/>
-       <br/> <br/> <br/> <br/> <br/> <br/> <br/>
-       <br/>
-       <div class="container mt-5" id="sdc">
-    <form method="Post" action="">
-        <div class="form-group">
-            <label class="prod-form" for="name">Your Name</label>
-            <input name="userName" type="text" class="form-control" id="name" placeholder="Enter your Name"
-                required value={this.state.name}
-                onChange={this.onChangeName}/>
-        </div>
-        
-        <div class="form-group">
-            <label class="prod-form" for="Email">Email address</label>
-            <input name="Email" type="email" class="form-control" id="Email" placeholder="Enter your email"
-                required value={this.state.email}
-                onChange={this.onChangeEmail}/>
-        </div>
-        <div class="form-group">
-            <label class="prod-form" for="date">Date</label>
-            <input type="date" class="form-control" id="date" name="Date1" placeholder="YYYY-MM-DD" required
-                pattern="(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])/(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])/(?:30))|(?:(?:0[13578]|1[02])-31))"
-                title="Enter a date in this format YYYY/MM/DD" />
-        </div>
-        <div class="form-group">
-            <label class="prod-form" for="text">Subject</label>
-            <input name="Num" type="text" class="form-control" id="phonenum" placeholder="Enter the subject "
-                required  value={this.state.subject}
-                onChange={this.onChangeSubject}/>
-        </div>
-         <div class="form-group">
-        <label  class="prod-form" for="text">Message Content   </label>
-         <input  type="text" name="message"  class="form-control" placeholder=" Type your Message here "
-           value={this.state.message}
-           onChange={this.onChangeMessage} />
-         </div>
-<br/>
-         <div class="row justify-content-center eee p-4">
-            <a href="" class="neewwww"> 
-            <button type="submit" class="btn btn-outline-success order-btt"  onClick={e => this.onSubmit(e)} >Submit</button>
-            </a>
-        </div>
-         <div class="brief-container"></div>
-   </form>
+   <Fragment>
+
+
+
+   </Fragment>
    
-</div>
-<div class="sections">
-  <img  alt=" contact us" src="img\clipart-phone-black-and-white-9.png" style={{width:500,height:500,float:'right',padding:10,margintop:30,alignContent:'right'}}/>
-  </div>
-<br/>
-       <br/> <br/> <br/> <br/> <br/> <br/> <br/>
-       
-</div>
   );
 }
 }
