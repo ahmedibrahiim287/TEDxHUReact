7c79defa86df5351a4901754d4aa8c93e5a7a6ac
Maryam <mariamly98@yahoo.com>
2021-03-31 19:46:22 +0300

    7.7

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -42,66 +42,97 @@ export class Contactus extends Component {
    /* console.log(obj) */
 
    this.setState({name:'', email:'', subject:'', message:''})
+ };
+ constructor(){
+   alert("Sorry, We're Not Avaliable now. ")
 
- }
+ };
 
   render() {
   return (
-    <div class="container">
-       <br/>
-       <br/>
+  
+<div className="container">
+       <div className="row " id="contatti">
+    <div className="container mt-5 " >
+    
+        <div class="row" style={{height:550}}>
+         
+          <div className="thanks-photo lefti">
+            <h2 className="text-uppercase mt-3 font-weight-bold text-black">Contact Us</h2>
+            <form action="">
+              <div className="row">
+                <div className="col-lg-6">
+                  <div className="form-group">
+                  <label class="prod-form" for="name">Your Name</label>
+                    <input type="text" className="form-control mt-2" placeholder="Name" 
+                     required value={this.state.name}  onChange={this.onChangeName}/>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="form-group">
+                  <label class="prod-form" for="Email">Email address</label>
+                    <input type="email" className="form-control mt-2" placeholder="Email" 
+                    required value={this.state.email} onChange={this.onChangeEmail}/>
+                  </div>
+                </div>
+                <div className="col-lg-6">
+                  <div className="form-group">
+                    <input type="text" className="form-control mt-2" placeholder="subject" 
+                      required  value={this.state.subject}
+                      onChange={this.onChangeSubject}/>
+                  </div>
+                </div>
+                
+                <div className="col-lg-6">
+                  <div className="form-group">
+                    <input type="phone" className="form-control mt-2" placeholder="phone number" required/>
+                  </div>
+                </div>
+                <div className="col-12">
+                  <div className="form-group">
+                    <textarea className="form-control" id="exampleFormControlTextarea1" placeholder="your massage" rows="3" required
+                   value={this.state.message} onChange={this.onChangeMessage} ></textarea>
+                  </div>
+                </div>
+
+                <div className="wrapper col-6">
+                  <a href=""><button type="submit" className=" btn--border btn-read btn--animated"onClick={e => this.onSubmit(e)} >SUBMIT</button></a>
+                </div>    
+
+            </div>
+            </form>
+            </div> 
+            
+          <div className=" thanks-photo righti " >
+            {/* <img  src="/img/clipart-phone-black-and-white-9.png" alt="contact-us" width="500" height="600"/> */}
+
+            <div classNameName="wrapper row">
+        <a onClick={}><button type="button" className=" btn--border btn-read btn--animated">volanter</button></a>
+        <a onClick={}><button type="button" className=" btn--border btn-read btn--animated">speaker</button></a>
+      </div>
+     
+              
+            
+
+            
+            <div className="brief-container">
+               <br/>
+            <h2 className="text-uppercase mt-4 font-weight-bold">find us</h2>
+            <div className="my-4">
+                <a href=" https://www.facebook.com/TEDxHUni/"><i class="fab fa-facebook fa-3x"></i></a>
+                <a href="https://www.linkedin.com/company/tedxhuni/"><i class="fab fa-linkedin fa-3x"></i></a>
+                {/* <a href=""><i class="fab fa-instagram-square "></i></a>  */}
+                <a href="https://www.youtube.com/user/TEDxTalks/videos"><i class="fab fa-youtube fa-3x"></i></a>
+                <a href="https://www.instagram.com/TEDxHUni/"><i class="fab fa-instagram fa-3x"></i></a>
+                </div>
+            <i className="fas fa-phone mt-3"></i> <a href="tel:+">+201158567766</a><br/>
+            <i className="fa fa-envelope mt-3"></i> <a href="Tedxhelwanuniversity@gmail.com">Tedxhelwanuniversity@gmail.com</a><br/>
+          
+            </div> </div> </div>
+              <br/>
        <br/> <br/> <br/> <br/> <br/> <br/> <br/>
-       <br/>
-       <div class="container mt-5" id="sdc">
-    <form method="Post" action="">
-        <div class="form-group">
-            <label class="prod-form" for="name">Your Name</label>
-            <input name="userName" type="text" class="form-control" id="name" placeholder="Enter your Name"
-                required value={this.state.name}
-                onChange={this.onChangeName}/>
-        </div>
-        
-        <div class="form-group">
-            <label class="prod-form" for="Email">Email address</label>
-            <input name="Email" type="email" class="form-control" id="Email" placeholder="Enter your email"
-                required value={this.state.email}
-                onChange={this.onChangeEmail}/>
-        </div>
-        <div class="form-group">
-            <label class="prod-form" for="date">Date</label>
-            <input type="date" class="form-control" id="date" name="Date1" placeholder="YYYY-MM-DD" required
-                pattern="(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])/(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])/(?:30))|(?:(?:0[13578]|1[02])-31))"
-                title="Enter a date in this format YYYY/MM/DD" />
-        </div>
-        <div class="form-group">
-            <label class="prod-form" for="text">Subject</label>
-            <input name="Num" type="text" class="form-control" id="phonenum" placeholder="Enter the subject "
-                required  value={this.state.subject}
-                onChange={this.onChangeSubject}/>
-        </div>
-         <div class="form-group">
-        <label  class="prod-form" for="text">Message Content   </label>
-         <input  type="text" name="message"  class="form-control" placeholder=" Type your Message here "
-           value={this.state.message}
-           onChange={this.onChangeMessage} />
-         </div>
-<br/>
-         <div class="row justify-content-center eee p-4">
-            <a href="" class="neewwww"> 
-            <button type="submit" class="btn btn-outline-success order-btt"  onClick={e => this.onSubmit(e)} >Submit</button>
-            </a>
-        </div>
-         <div class="brief-container"></div>
-   </form>
-   
-</div>
-<div class="sections">
-  <img  alt=" contact us" src="img\clipart-phone-black-and-white-9.png" style={{width:500,height:500,float:'right',padding:10,margintop:30,alignContent:'right'}}/>
-  </div>
-<br/>
-       <br/> <br/> <br/> <br/> <br/> <br/> <br/>
-       
-</div>
+      </div> 
+       </div></div>
   );
 }
 }
