7c13d87b9a153b3cf3d8983564e2c653e61d3fba
nasr1231 <nasr.1547474@gmail.com>
2021-04-02 18:42:20 +0200

    new in home and some edits

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,7 +16,7 @@ import 'react-awesome-slider/dist/styles.css';
 <div>
 
   
-<div class="alrazy-title" data-splitting>Welcome To TEDx Helwan university 2021</div>
+<div className="alrazy-title" data-splitting>Welcome To <span className="ted-color">TEDx</span> Helwan University</div>
 <script src="https://unpkg.com/splitting@1.0.0/dist/splitting.js"></script>
 <script>
     Splitting();
@@ -34,7 +34,16 @@ import 'react-awesome-slider/dist/styles.css';
     <div data-src="\img\slider\cover-1.png" />
   </AwesomeSlider>
   </div>
-
+  <div className="brief-conatiner ">
+    
+    <h2 className="helwan-head" style={{textAlign:"center"}}><i className="arrow right"></i> About <span className="ted-color">Tedx</span>HelwanUniversity</h2>
+    <p>An independently organized TED event in Helwan
+        Proceeding from our belief in the power of ideas, we determined to create a community of ideas aiming to enrich our community minds.
+        Also inspiring and motivating them to cope with and contribute to the age of renaissance that our country lives in recently,
+        hoping to get out and reach the global edge of success starting from Helwan University.
+    </p>
+  </div>
+  <div className="brief-conatiner "></div>
 
   <div className="grid-holder">
           <div className="grid-box">
@@ -51,10 +60,7 @@ import 'react-awesome-slider/dist/styles.css';
               </h5>
           </div>
           <div className="grid-box">
-          <h5  className="calco">
-            Speakers <br></br>
-            43
-            </h5>
+          <h5  className="calco">Speakers <br></br>43</h5>
           </div>
           <div className="grid-box">
           <h5 className="calco">
@@ -62,52 +68,58 @@ import 'react-awesome-slider/dist/styles.css';
               18
               </h5>                  
           </div>
-  
-          <tbody>
-  
-          </tbody>
       </div>
+      <div className="brief-conatiner "></div>
+      <section className="padd container boot-elemant-bg py-md-5 py-4  " >
     
-
-  
-    
-    <div className="brief-conatiner ">
-    
-        <h2 className="helwan-head"><i className="arrow right"></i> About <span className="ted-color">Tedx</span>HelwanUniversity</h2>
-        <p>An independently organized TED event in Helwan
-            Proceeding from our belief in the power of ideas, we determined to create a community of ideas aiming to enrich our community minds.
-            Also inspiring and motivating them to cope with and contribute to the age of renaissance that our country lives in recently,
-            hoping to get out and reach the global edge of success starting from Helwan University.
-        </p>
-    </div>
-
+    <div className="container position-relative py-md-5 py-0">
     <div className="brief-conatiner "></div>
-    <div class="container">
-    <div class="row text-center">
-    <div class = "centered">
-    <div class = "blob-1"></div>
-    <div class = "blob-2"></div>
+    <div className="brief-conatiner "></div>
+
+                <div className="row">
+            <div className="evicon col-lg-8">
+                <span className="text-black text-uppercase  "> </span>
+                <h2 className="text-white display-3 font-weight-bold"> </h2>
+                <p className="f-w-16 mb-4"> </p>
+                
+                <a target="_blank" href=" https://www.ted.com/tedx/events/40335" className="btn btn-outline-white btn-lg px-4"  >TED <i className="fas fa-times" style={{color: "red"}}></i> </a>
+                <a target="_blank" href=" https://www.facebook.com/events/3336545329780286/" className="btn btn-outline-white btn-lg px-4"> <i className="fab fa-facebook-f"></i> </a>
+                <a target="_blank" href=" https://www.youtube.com/playlist?list=PLwCKOAvD-tKnyzziugxDDquCA1-Pi-42t" className="btn btn-outline-white btn-lg px-4"> <i className="fab fa-youtube"></i> </a>
+                <a target="_blank" href="./Tangram " className="btn btnn  btn-outline-white btn-lg px-4"> Read more </a>
+
+            </div>
+        </div>
+    </div>
+    <div className="elemant-bg-overlay black"></div>
+</section>
+    <div className="brief-conatiner "></div>
+    <div className="container">
+    <div className="row text-center">
+    <div className = "centered">
+    <div className = "blob-1"></div>
+    <div className = "blob-2"></div>
 </div>
     </div>
 </div>
-    <div id="countdown" class="container">
+
+    <div id="countdown" className="container">
   <h1 id="headline">Countdown To Our Next Event:</h1>
   <div id="countdown">
     <ul>
-      <li class="cd"><span id="days"></span>days</li>
-      <li class="cd"><span id="hours"></span>Hours</li>
-      <li class="cd"><span id="minutes"></span>Minutes</li>
-      <li class="cd"><span id="seconds"></span>Seconds</li>
+      <li className="cd"><span id="days"></span>days</li>
+      <li className="cd"><span id="hours"></span>Hours</li>
+      <li className="cd"><span id="minutes"></span>Minutes</li>
+      <li className="cd"><span id="seconds"></span>Seconds</li>
     </ul>
   </div>
-  <div class="message">
+  <div className="message">
   </div>
 </div>
 <div className="latest-speakers">
-    <span className="latest-speakers-title">Our Favorite Speakers</span>
+    <span className="latest-speakers-title">Our Favorite <span className="ted-color">Speakers</span></span>
     <div className="latest-speakers-title-dash"><span>_____________</span></div>
 </div>
-<div className="speakers-holder">
+<div className="speakers-holder" id="home-speakers-holder">
     <div className="speaker-element">
         <img src="\img\speakers\andrewashraf.jpg" alt="" className="image" />
         <div className="brief-middle">
@@ -151,8 +163,48 @@ import 'react-awesome-slider/dist/styles.css';
           </div>
         </div>
       </div>
+      <div className="speaker-element">
+        <img src="\img\speakers\mohamed.jpg" alt="Mahmoud Abd Elmageed" className="image" />
+        <div className="brief-middle">
+          <div className="speaker-context-title">Mahmoud Abd Elmageed</div>
+          <div className="speaker-context">Founder of Have A Dream and Co-Founder of Schema Advertising</div>
+          <div className="speaker-context">
+            <a href="https://youtu.be/g9sHfsLbsxA" rel="noreferrer" target="_blank">See His Talk</a>
+          </div>
+        </div>
+      </div>
+      <div className="speaker-element">
+        <img src="\img\speakers\mohamed.jpg" alt="Mahmoud Abd Elmageed" className="image" />
+        <div className="brief-middle">
+          <div className="speaker-context-title">Mahmoud Abd Elmageed</div>
+          <div className="speaker-context">Founder of Have A Dream and Co-Founder of Schema Advertising</div>
+          <div className="speaker-context">
+            <a href="https://youtu.be/g9sHfsLbsxA" rel="noreferrer" target="_blank">See His Talk</a>
+          </div>
+        </div>
+      </div>
+      <div className="speaker-element">
+        <img src="\img\speakers\mohamed.jpg" alt="Mahmoud Abd Elmageed" className="image" />
+        <div className="brief-middle">
+          <div className="speaker-context-title">Mahmoud Abd Elmageed</div>
+          <div className="speaker-context">Founder of Have A Dream and Co-Founder of Schema Advertising</div>
+          <div className="speaker-context">
+            <a href="https://youtu.be/g9sHfsLbsxA" rel="noreferrer" target="_blank">See His Talk</a>
+          </div>
+        </div>
+      </div>
+      <div className="speaker-element">
+        <img src="\img\speakers\mohamed.jpg" alt="Mahmoud Abd Elmageed" className="image" />
+        <div className="brief-middle">
+          <div className="speaker-context-title">Mahmoud Abd Elmageed</div>
+          <div className="speaker-context">Founder of Have A Dream and Co-Founder of Schema Advertising</div>
+          <div className="speaker-context">
+            <a href="https://youtu.be/g9sHfsLbsxA" rel="noreferrer" target="_blank">See His Talk</a>
+          </div>
+        </div>
+      </div>
+      
 </div>
-    <div className="brief-conatiner "></div>
         {/* 
            __   
         .__(.)< (Buck Buck)
